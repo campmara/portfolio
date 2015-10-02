721cdef8e2c96298216396d35f4d3b2d88e6cfda
Murray Campbell <MurrayIRC@gmail.com>
2015-10-02 14:50:43 -0500

    Set up express and Angular.

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello, ______");
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.use(express.static('web'));
+
+// app.get('/', function(req, res) {
+//  res.send('ha');
+// })
+
+app.listen(3000);
