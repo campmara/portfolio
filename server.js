976264b6cf7bd3914e63115a53a5a746546efa66
Murray Campbell <MurrayIRC@gmail.com>
2016-01-28 19:50:57 -0600

    update

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 80;
 
-var cleanUrls = require('clean-urls');
-
-app.use(cleanUrls(['/web/index.html']));
-//app.use(express.static('web'));
+app.use(express.static('web'));
 
 console.log(port);
 app.listen(port);
