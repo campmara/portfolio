f6acb9366bef4f21f4eda3ec2a57d593288bf4fa
Murray Campbell <MurrayIRC@gmail.com>
2016-01-28 19:11:59 -0600

    update

--- web/app.js
+++ web/app.js
@@ -6,7 +6,41 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
+])
+	.config(['$routeProvider', function($routeProvider) {
+
+		$routeProvider
+            .when('/', {
+                templateUrl : '/index.html',
+                controller : mainController
+            })
+            .when('/games', {
+                templateUrl : '/games.html',
+                controller : mainController
+            })
+            .when('/music', {
+                templateUrl : '/music.html',
+                controller : mainController
+            });
+            .when('/writing', {
+                templateUrl : '/writing.html',
+                controller : mainController
+            });
+            .otherwise({redirectTo: '/view1'});
+
+            // use the HTML5 History API
+        	$locationProvider.html5Mode(true);
+	}]);
+
+
+/*
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+*/
