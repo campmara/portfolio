54db76299b2c32a575bfd6a42a39eb07b99ce1a6
Murray Campbell <MurrayIRC@gmail.com>
2016-01-28 20:06:33 -0600

    update

--- server.js
+++ server.js
@@ -6,5 +6,17 @@ var express = require('express'),
 
 app.use(express.static('web'));
 
+app.get('/games', function(req,res){
+ res.sendfile(__dirname + '/web/games.html');
+}); 
+
+app.get('/music', function(req,res){
+ res.sendfile(__dirname + '/web/music.html');
+}); 
+
+app.get('/writing', function(req,res){
+ res.sendfile(__dirname + '/web/writing.html');
+}); 
+
 console.log(port);
 app.listen(port);
