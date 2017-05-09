644da47a8f235cf16cd38c6b84f358be34f34bfe
Murray Campbell <MurrayIRC@gmail.com>
2017-05-09 17:29:36 -0500

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('web'));
 
 app.listen(app.get('port'), function() {
